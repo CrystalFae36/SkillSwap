1b55815e57fa447076ccb2cf305fa5d8573a5327
Seum <26SeumP@mydacc.org>
2025-09-22 12:30:36 -0400

    added a bunch of necessary files - P, 12:30PM, 9/22/2025

---
+++ student-login.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SkillSwap - Student Login</title>
+</head>
+<body>
+    
+</body>
+</html>
