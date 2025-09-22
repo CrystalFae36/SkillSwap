0279c9b13d6425f1ce4f9db3e1d90bad183556eb
Seum <26SeumP@mydacc.org>
2025-09-22 14:17:26 -0400

    added more pages - P, 2:17 PM 9/22/2025

---
+++ User.php
@@ -0,0 +1,24 @@
+<?php
+    require_once 'DatabaseConnection.php';
+
+    class User{
+        //Fields
+        public $id = 0;
+        public $username = "";
+        public $displayname = "";
+        public $firstname = "";
+        public $lastname = "";
+        public $email = "";
+        public $state_id = 0;
+        public $isadmin = 0;
+        public $createdate;
+
+
+
+
+    }
+
+
+
+
+?>
--- forums.php
+++ forums.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SkillSwap - Standalone Login</title>
+    <title>Document</title>
 </head>
 <body>
     
